5ac635e67d3e8d041b2eefd706b499f822f6fdf6
tct <jznctt@gmail.com>
2022-07-17 16:50:23 +0700

    ConstAndLet

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -1,11 +1,17 @@
-// Overview Statement
+// Overview Syntax
 let q, e, r;
 q = 2;
 e = 6;
 r = q * 2 + e;
 console.log(r);
-
-// Syntax
 a = ('Thuận');
 y = ('Thiên');
 console.log(a,y);
+// const can't change the value
+const c = 100
+C = c + 1
+console.log(C)
+// but let can
+let b = 99
+b = b + 1
+console.log(b)
