50f8bc1d4e8c38f7640241dc82ecb03ee2a8f5c5
tct <jznctt@gmail.com>
2022-07-17 16:39:20 +0700

    LetandConst

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -1,5 +1,11 @@
+// Overview Statement
 let q, e, r;
-q = 2
-e = 6
-r = q * 2 + e
-console.log(r)
+q = 2;
+e = 6;
+r = q * 2 + e;
+console.log(r);
+
+// Syntax
+a = ('Thuận');
+y = ('Thiên');
+console.log(a,y);
